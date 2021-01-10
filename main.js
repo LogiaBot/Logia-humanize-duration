3056d4702faf2ae31f5d95044617258e13491377
zechaos031 <delrieu.adrien@gmail.com>
2021-01-10 14:32:12 +0100

    :wrench: Update gitignore

--- main.js
+++
@@ -1,7 +0,0 @@
-const discord = require('discord.js')
-const client = new discord.Client()
-const hd = require('./humanize-duration')
-
-client.on('ready' , ()=>{
-console.log(hd(client.uptime+1005*5))
-})
