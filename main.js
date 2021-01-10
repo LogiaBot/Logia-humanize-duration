5b1e746b5e90ef210eb3a07a7ed1260d0586d132
zechaos031 <delrieu.adrien@gmail.com>
2021-01-10 14:24:33 +0100

    :wrench: Some update

--- main.js
+++ main.js
@@ -5,4 +5,3 @@ const hd = require('./humanize-duration')
 client.on('ready' , ()=>{
 console.log(hd(client.uptime+1005*5))
 })
-client.login('NzE3NzE2Mzk3OTI0Mjg2NTc0.XtpK7Q.VNNSUEPY0MIOFMvdCg_dAvK7Ig8')
